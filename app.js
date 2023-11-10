7e25d1127431f54649c001d44a5f2114a8658e33
fctico11 <fac322@nyu.edu>
2023-11-10 12:04:12 -0500

    nav bar styling

--- app.js
+++ app.js
@@ -130,12 +130,12 @@ app.get('/logout', (req, res) => {
 });
 
 // Start the server
-/*
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-*/
+
 
 
 module.exports = app;
